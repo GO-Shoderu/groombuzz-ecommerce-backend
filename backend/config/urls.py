febad38ae2508e5cde7acace2f122b95ac49f7fc
GO-Shoderu <go.shoderu@gmail.com>
2026-02-15 09:37:59 +0200

    Adding Auth endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # JWT Auth
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    # APIs
+    path("api/", include("catalog.urls")),
+    path("api/", include("transactions.urls")),
+    path("api/", include("engagement.urls")),
 ]
