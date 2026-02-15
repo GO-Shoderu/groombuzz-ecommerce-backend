6929aabc4be8aa2147a9027604264bfbade6cf69
GO-Shoderu <go.shoderu@gmail.com>
2026-02-15 10:11:16 +0200

    Putting validator for amount

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class TimeStampedModel(models.Model):
@@ -60,7 +61,13 @@ class Payment(TimeStampedModel):
         on_delete=models.CASCADE,
         related_name="payments",
     )
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)],
+    )
+
     payment_method = models.CharField(
         max_length=20,
         choices=Method.choices,
